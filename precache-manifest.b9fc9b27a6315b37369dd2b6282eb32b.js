ab785819c2137b1ac005fa63eaea6ba3675b8d17
Kristi Miller <kristi.ann.miller@gmail.com>
2020-11-21 14:00:26 -0700

    Updates

---
+++ precache-manifest.b9fc9b27a6315b37369dd2b6282eb32b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d8c07b177bc8ab268150d737f5bf5b3",
+    "url": "/rancid-tomatillos-km-am/index.html"
+  },
+  {
+    "revision": "297b0c2fb03a772396ed",
+    "url": "/rancid-tomatillos-km-am/static/css/main.1eb567c7.chunk.css"
+  },
+  {
+    "revision": "7f231a4ecc12456d59dd",
+    "url": "/rancid-tomatillos-km-am/static/js/2.c9d55733.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rancid-tomatillos-km-am/static/js/2.c9d55733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297b0c2fb03a772396ed",
+    "url": "/rancid-tomatillos-km-am/static/js/main.cf999685.chunk.js"
+  },
+  {
+    "revision": "6c4e6f57276aeded84dc",
+    "url": "/rancid-tomatillos-km-am/static/js/runtime-main.0704df9d.js"
+  },
+  {
+    "revision": "e309505f39ea9c83db54cce8b37c2eea",
+    "url": "/rancid-tomatillos-km-am/static/media/RancidTomLogo.e309505f.png"
+  }
+]);
